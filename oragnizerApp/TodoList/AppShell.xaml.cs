5daa400f4da07ba3377bf62584edec617ad09eb1
Michał Janik <mjanik45@gmail.com>
2022-06-09 22:43:45 +0200

    Add New Sprint view add in AppShell.xaml.cs

--- oragnizerApp/TodoList/AppShell.xaml.cs
+++ oragnizerApp/TodoList/AppShell.xaml.cs
@@ -14,7 +14,7 @@ namespace TodoList
             //Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
             //Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
             //Routing.RegisterRoute(nameof(NewCategoryPage), typeof(NewCategoryPage));
-            //Routing.RegisterRoute(nameof(NewServicemanPage), typeof(NewServicemanPage));
+            Routing.RegisterRoute(nameof(NewSprintPage), typeof(NewSprintPage));
             Routing.RegisterRoute(nameof(NewEmployeePage), typeof(NewEmployeePage));
             Routing.RegisterRoute(nameof(ClientDetailPage), typeof(ClientDetailPage));
             Routing.RegisterRoute(nameof(NewClientPage), typeof(NewClientPage));
