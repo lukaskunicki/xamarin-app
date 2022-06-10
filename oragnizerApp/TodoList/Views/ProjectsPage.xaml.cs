2275ec3125b544598b553e47e291982af2818e16
Michał Janik <mjanik45@gmail.com>
2022-06-10 22:48:37 +0200

    Add All Project view part 1

---
+++ oragnizerApp/TodoList/Views/ProjectsPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoList.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TodoList.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProjectsPage : ContentPage
+    {
+        ProjectsViewModel _viewModel;
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+        public ProjectsPage()
+        {
+            InitializeComponent();
+            BindingContext = _viewModel = new ProjectsViewModel();
+        }
+    }
+}
