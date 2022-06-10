7a005e1fd9beb9bf114b53e0f30d87e90b5e8de0
Michał Janik <mjanik45@gmail.com>
2022-06-10 18:11:17 +0200

    Add All Priorities view

---
+++ oragnizerApp/TodoList/Views/PrioritiesPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoList.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TodoList.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PrioritiesPage : ContentPage
+    {
+        PrioritiesViewModel _viewModel;
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+        public PrioritiesPage()
+        {
+            InitializeComponent();
+            BindingContext = _viewModel = new PrioritiesViewModel();
+        }
+    }
+}
