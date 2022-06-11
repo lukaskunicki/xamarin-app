4a624acdaa6150220d086277aab8105c5a779fa9
Michał Janik <mjanik45@gmail.com>
2022-06-12 01:21:56 +0200

    Fix Edit Ticket view

--- oragnizerApp/TodoList/Views/TicketDetailPage.xaml.cs
+++ oragnizerApp/TodoList/Views/TicketDetailPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using TodoList.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,7 @@ namespace TodoList.Views
         public TicketDetailPage()
         {
             InitializeComponent();
+            BindingContext = new TicketDetailViewModel();
         }
     }
 }
