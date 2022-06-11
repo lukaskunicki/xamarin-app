abd31e41d5523195bdc6c171303d0af6f2901771
Michał Janik <mjanik45@gmail.com>
2022-06-12 01:21:29 +0200

    Add Sprints Edit view

--- oragnizerApp/TodoList/ViewModels/SprintsViewModel.cs
+++ oragnizerApp/TodoList/ViewModels/SprintsViewModel.cs
@@ -95,7 +95,7 @@ namespace TodoList.ViewModels
             if (Sprint == null)
                 return;
             // This will push the CategoryDetailPage onto the navigation stack
-            //await Shell.Current.GoToAsync($"{nameof(ClientDetailPage)}?{nameof(ClientDetailViewModel.ClientId)}={client.ClientId}");
+            await Shell.Current.GoToAsync($"{nameof(SprintDetailPage)}?{nameof(SprintDetailViewModel.SprintId)}={Sprint.SprintId}");
         }
 
         async void DeleteSprintCommandHandler(Sprint Sprint)
